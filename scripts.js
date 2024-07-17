e4ccd61534de970c952eacb682fe2954fc471a29
Andy <ats71090@yahoo.com>
2024-07-17 14:50:14 -0400

    updates

--- scripts.js
+++ scripts.js
@@ -1,5 +1,28 @@
-const firstName = document.getElementById("first-name-input")
+const firstName = document.getElementById("first-name-input");
+console.log(firstName);
 
+const lastName = document.getElementById("last-name-input");
+console.log(lastName);
+
+const emailAddress = document.getElementById("email-input");
+console.log(emailAddress);
+
+const generalEnquiry = document.getElementById("general-enquiry-input");
+console.log(generalEnquiry);
+
+const supportRequest = document.getElementById("support-request-input");
+console.log(supportRequest);
+
+const messageInput = document.getElementById("message-input");
+console.log(messageInput);
+
+const consentInput = document.getElementById("consent");
+console.log(consentInput);
+
+const submitCTA = document.getElementById("submitCTA");
+console.log(submitCTA);
+
+/*
 firstName.addEventListener("input", () => {
   firstName.checkValidity();
   if (firstName.validity.typeMismatch) {
@@ -10,3 +33,4 @@ firstName.addEventListener("input", () => {
     console.log("Valid")
   }
 });
+*/
